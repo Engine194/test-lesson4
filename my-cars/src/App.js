5bf3280ec9201978237f4ee648c70da1e81f02f6
Nguyen Ngoc <ngoc.n@shopstack.asia>
2024-01-17 21:56:17 +0700

    render UI

--- my-cars/src/App.js
+++ my-cars/src/App.js
@@ -8,25 +8,52 @@ import { Car } from "./model/car";
 //   }
 // }
 
-const App = () => {
+// stateless >< statefull
+const sourceUrl = "https://655770eebd4bcef8b612a6b5.mockapi.io/api/v1/cars";
+const getData = async (url) => {
+  const carList = await fetch(url).then((res) => res.json());
+  if (carList?.length > 0) {
+    return carList.map((car) => new Car(car));
+  }
+  return [];
+};
+
+const App = (props) => {
   const [data, setData] = useState([]);
-  
+
+
   useEffect(() => {
-    const sourceUrl = "https://655770eebd4bcef8b612a6b5.mockapi.io/api/v1/cars";
-    const getData = async (url) => {
-      const carList = await fetch(url).then((res) => res.json());
-      if (carList?.length > 0) {
-        return carList.map((car) => new Car(car));
-      }
-      return [];
-    };
-    getData(sourceUrl).then((cars) => setData(cars));
+    getData(sourceUrl).then((cars) => {
+      console.log('setData...');
+      setData(cars);
+    });
+    console.log('useEffect...');
   }, []);
 
+  const handleEdit = (id) => {
+    const newData = data.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          name: "nikita",
+        };
+      } else {
+        return item;
+      }
+    });
+    setData(newData);
+  };
+
+  const handleDelete = (id) => {
+    const newData = data.filter((item) => item.id !== id);
+    setData(newData);
+  };
+  
   return (
     <>
       <div>
         <table>
+          {console.log('render....', data)}
           <thead>
             <tr>
               <th>#</th>
@@ -35,11 +62,27 @@ const App = () => {
               <th>Year</th>
               <th>Max Velocity</th>
               <th>Acceleration</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
-            {data.map((item) => {
-              const { id, name, year, maxVelocity, acceleration } = item;
+            {data.map((item, index) => {
+              const { id, name, brand, year, maxVelocity, acceleration } = item;
+              return (
+                <tr key={index}>
+                  <td>{id}</td>
+                  <td>{name}</td>
+                  <td>{brand}</td>
+                  <td>{year}</td>
+                  <td>{maxVelocity}</td>
+                  <td>{acceleration}</td>
+                  <td>
+                    <button onClick={() => handleEdit(id)}>Edit</button>
+
+                    <button onClick={() => handleDelete(id)}>Delete</button>
+                  </td>
+                </tr>
+              );
             })}
           </tbody>
         </table>
