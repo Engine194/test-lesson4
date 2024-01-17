fefdd5f8165c1e82a3adff6b34d72fea8e59b048
Nguyen Ngoc <ngoc.n@shopstack.asia>
2024-01-17 19:42:17 +0700

    add tesst

---
+++ test.js
@@ -0,0 +1,32 @@
+// Ex1: === and == operator
+// Nêu ví dụ về so sánh == và === để thấy được cái nào chặt chẽ hơn
+
+// Ex2: Ko chay code viet ra ket qua
+
+undefined == null
+undefined === null
+
+'' == undefined
+'' === undefined
+
+'' == null
+'' === null
+
+0 == ''
+0 === ''
+
+// Ex3: Tạo một class tên là Car gồm các thuộc tính
+// id, name, brand, year, maxVelocity, acceleration
+
+// Ex4: Cho một array sau, hãy dựa trên array này,
+// tạo ra một array mới với toàn bộ các phần tử là các instance của Car
+const sourceURL = 'https://655770eebd4bcef8b612a6b5.mockapi.io/api/v1/cars';
+
+
+// Ex5: Tạo một project reactjs mới với tên project là my-cars
+// tại trang chủ copy danh sách cars đã có ở bài 4 sang và hiển thị danh sách cars theo bảng với các cột
+
+// id | name | brand | year | acceleration | max velocity
+
+
+
